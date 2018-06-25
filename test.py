adff53ee57c2fe2e9f563f400478351bf740d0f4
Berchem <low118profile@gmail.com>
2018-06-26 03:08:37 +0800

    v0.1.0

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
-import numpy as np
 import pandas as pd
 import logistic_regression
+from logistic_regression.algorithm import newton
 
 
+# import data
 DF = pd.read_csv("binary.csv")
-data = np.array(DF)
-y = (data[:, 0:1] - 0.5) * 2  # outcome 0, 1 -> -1, 1
-x = np.concatenate((np.ones((data.shape[0], 1)), data[:, 1:]), axis=1)
 
-logist_fit = logistic_regression.fit()
-w, H = logist_fit.newton(x, y)
+# get data label
+col = DF.columns.values
 
-print w
+# outcome y
+y = DF[["admit"]]
+
+# variables x
+x = DF.loc[:, col[col != 'admit']]
+
+# logist_fit = logistic_regression.fit("newton", x, y)
+logist_fit = newton(x, y)
+
+
+print "  ".join("%.4f" % w for w in logist_fit.weight)
