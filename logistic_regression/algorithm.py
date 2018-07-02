0af4a690b7490d0061920a8216ca3e852cd94b0e
Berchem <low118profile@gmail.com>
2018-07-02 12:26:06 +0800

    v0.0.3

--- logistic_regression/algorithm.py
+++ logistic_regression/algorithm.py
@@ -10,7 +10,7 @@ class newton(LogisticRegression):
         w_new = np.zeros((n, 1))
         ntime = 0
         while ntime < self.iter_lim:
-            # updata parameter
+            # update parameter
             w_old = w_new
 
             # calculate diagonal matrix, that a is the A_ii
@@ -26,6 +26,42 @@ class newton(LogisticRegression):
 
             # calculate current parameter
             w_new = np.linalg.inv(H).dot(XAz)
+
+            # calculate tolerance and iteration time to break loop
+            if np.sqrt((w_new - w_old).T.dot(w_new - w_old)) < tol:  # all(abs(w - w_old) < tol):
+                break
+
+            else:
+                ntime += 1
+
+        # get parameter
+        self.weight = w_new
+
+
+class coord(LogisticRegression):
+    def __init__(self, x, y, lamb=1, tol=1e-8, iter_lim=100, fit_intercept=True):
+        LogisticRegression.__init__(self, x, y, lamb, tol, iter_lim, fit_intercept)
+        _, n = self.x.shape
+        w_new = np.zeros((n, 1))
+        ntime = 0
+        while ntime < self.iter_lim:
+            # updata parameter
+            w_old = w_new
+
+            # calculate diagonal matrix, that a is the A_ii
+            a = sigmoid_prime(self.x.dot(w_old))
+
+            g = []
+            axx =[]
+            for k, _ in enumerate(w_old):
+                for i, _ in enumerate(self.x):
+                    g += [1 - sigmoid(self.y[i] * np.dot(self.x[i, :], w_old)) * self.y[i] * self.x[i, k]]
+                    axx += a[i] * self.x[i, k] ** 2
+                g = sum(g) - lamb * w_old[k]
+                w_new[k] = w_old[k] + g / (lamb + sum(axx))
+
+            # calculate current parameter
+            # w_new = w_old
 
             # calculate tolerance and iteration time to break loop
             if np.sqrt((w_new - w_old).T.dot(w_new - w_old)) < tol:  # all(abs(w - w_old) < tol):
